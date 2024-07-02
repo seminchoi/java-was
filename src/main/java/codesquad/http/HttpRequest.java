b4f1c6b4504f8099295016534027419b9bff6fe2
seminchoi <choicco89@gmail.com>
2024-07-02 15:37:22 +0900

    feat: RequestLine을 클래스로 분리

--- src/main/java/codesquad/http/HttpRequest.java
+++ src/main/java/codesquad/http/HttpRequest.java
@@ -3,57 +3,47 @@ package codesquad.http;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
     private static final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
 
-    private static Map<String, String> headers = new HashMap<>();
+    private HttpRequestLine requestLine;
+    private Map<String, String> headers = new HashMap<>();
 
-    public HttpRequest(final InputStream inputStream) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        String request = reader.readLine();
-        logger.info(request.toString());
-        parseRequestLine(request);
-
-        if (reader.ready()) {
-            parseRequestHeader(reader.readLine());
+    public HttpRequest(List<String> request) throws IOException {
+        if(request.isEmpty()){
+            return;
         }
+        this.requestLine = new HttpRequestLine(request.get(0));
+        parseRequestHeader(request);
     }
 
-    private void parseRequestLine(final String requestLine) {
-        String[] requestLineArgs = requestLine.split(" ");
 
-        headers.put("Method", requestLineArgs[0]);
-        headers.put("Uri", requestLineArgs[1]);
-        headers.put("HttpVersion", requestLineArgs[2]);
-    }
-
-    private void parseRequestHeader(final String requestHeader) {
-        String[] requestHeaderArgs = requestHeader.split(":");
-        try {
-            headers.put(requestHeaderArgs[0], requestHeaderArgs[1]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            logger.error(requestHeader);
+    private void parseRequestHeader(final List<String> request) {
+        for (int line = 1; line < request.size() && !request.get(line).equals("\r\n") ; line++) {
+            String[] requestHeaderArgs = request.get(line).split(":");
+            try {
+                headers.put(requestHeaderArgs[0], requestHeaderArgs[1]);
+            } catch (ArrayIndexOutOfBoundsException e) {
+                logger.error("{} 리퀘스트 헤더에 디렉티브가 없습니다.", request.get(line));
+            }
         }
     }
 
     public HttpMethod getMethod() {
-        return HttpMethod.valueOf(headers.get("Method"));
+        return requestLine.getMethod();
     }
 
     public String getUri() {
-        return headers.get("Uri");
+        return requestLine.getUri();
     }
 
     public String getHttpVersion() {
-        return headers.get("HttpVersion");
+        return requestLine.getHttpVersion();
     }
 
     public String getHost() {
@@ -66,5 +56,9 @@ public class HttpRequest {
 
     public String getAccept() {
         return headers.get("Accept");
+    }
+
+    public boolean isValid() {
+        return requestLine != null;
     }
 }
