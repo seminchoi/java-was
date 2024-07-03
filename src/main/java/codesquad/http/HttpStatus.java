495d5e18541f16f0c17cc05b4b04cbae82e94d4c
seminchoi <choicco89@gmail.com>
2024-07-03 21:26:20 +0900

    feat: HttpStatus 500 상태코드 추가

--- src/main/java/codesquad/http/HttpStatus.java
+++ src/main/java/codesquad/http/HttpStatus.java
@@ -3,7 +3,8 @@ package codesquad.http;
 public enum HttpStatus {
     OK(200, "OK"),
     BAD_REQUEST(400, "BAD REQUEST"),
-    NOT_FOUND(404, "NOT FOUND"),;
+    NOT_FOUND(404, "NOT FOUND"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),;
 
     private final int statusCode;
     private final String status;
