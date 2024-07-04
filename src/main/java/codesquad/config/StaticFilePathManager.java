6bb23436a9e973388e83ed5eceefd5ff7329edce
seminchoi <choicco89@gmail.com>
2024-07-05 01:01:59 +0900

    refactor: 클래스 ResourcePathManager -> StaticFilePathManager로 네이밍 변경

--- src/main/java/codesquad/config/StaticFilePathManager.java
+++ src/main/java/codesquad/config/StaticFilePathManager.java
@@ -2,7 +2,7 @@ package codesquad.config;
 
 import java.util.List;
 
-public class ResourcePathManager {
+public class StaticFilePathManager {
     private static final List<String> filePaths = List.of("src/main/resources/static");
 
     public List<String> getFilePaths() {
