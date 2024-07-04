cacb9b2f3084c817366c1131b80c746c96b3a4cb
seminchoi <choicco89@gmail.com>
2024-07-05 01:12:29 +0900

    feat: User 클래스 유효성 검증 구현

--- src/main/java/codesquad/domain/user/User.java
+++ src/main/java/codesquad/domain/user/User.java
@@ -1,11 +1,20 @@
 package codesquad.domain.user;
 
+import codesquad.exception.HttpException;
+import codesquad.http.HttpStatus;
+
 public class User {
     private String userId;
     private String password;
     private String name;
 
     public User(String userId, String password, String name) {
+        //TODO: 도메인객체가 Http 예외나 상태코드를 알 필요는 없다
+        if (userId == null || userId.isEmpty()
+                || password == null || password.isEmpty()
+                || name == null || name.isEmpty()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST);
+        }
         this.userId = userId;
         this.password = password;
         this.name = name;
