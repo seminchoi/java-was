0c2db4b4ac7d8fef0196d555b4b229fd9786cfe2
seminchoi <choicco89@gmail.com>
2024-07-05 01:03:28 +0900

    feat: 정적 경로로 HttpResponse를 생성하는 StaticFileProcessor 클래스 구현

---
+++ src/main/java/codesquad/config/StaticFileProcessor.java
@@ -0,0 +1,41 @@
+package codesquad.config;
+
+import codesquad.http.FileReader;
+import codesquad.http.HttpResponse;
+import codesquad.http.HttpStatus;
+
+import java.io.File;
+
+public class StaticFileProcessor {
+    private final StaticFilePathManager staticFilePathManager;
+
+    public StaticFileProcessor(StaticFilePathManager staticFilePathManager) {
+        this.staticFilePathManager = staticFilePathManager;
+    }
+
+    public HttpResponse readFile(String path) {
+        File file = null;
+
+        for (String defaultPath : staticFilePathManager.getFilePaths()) {
+            String absolutePath = System.getProperty("user.dir") + File.separator + defaultPath + path;
+
+            File curFile = new File(absolutePath);
+
+            if (curFile.exists()) {
+                file = curFile;
+                break;
+            }
+        }
+        if (file == null) {
+            return null;
+        }
+
+        FileReader fileReader = new FileReader(file);
+
+        HttpResponse httpResponse = new HttpResponse(HttpStatus.OK);
+        httpResponse.writeHeader("Content-Type", fileReader.getContentType().getDirective());
+        httpResponse.writeBody(fileReader.getBytes());
+
+        return httpResponse;
+    }
+}
