ad8fb4fd60c3dd862e4a0393f18784c673cede09
seminchoi <choicco89@gmail.com>
2024-07-05 01:00:31 +0900

    feat: 요청 메소드, 경로에 따라서 핸들러를 매칭할 수 있는 RouteEntry 클래스 구현

---
+++ src/main/java/codesquad/config/RouteEntry.java
@@ -0,0 +1,50 @@
+package codesquad.config;
+
+import codesquad.http.HttpMethod;
+import codesquad.http.HttpRequest;
+import codesquad.http.HttpResponse;
+
+import java.util.function.Function;
+
+public class RouteEntry {
+    private final HttpMethod method;
+    private final String path;
+    private final Function<HttpRequest, HttpResponse> handler;
+
+    public RouteEntry(HttpMethod method, String path, Function<HttpRequest, HttpResponse> handler) {
+        this.method = method;
+        this.path = path;
+        this.handler = handler;
+    }
+
+    public static class Builder {
+        private HttpMethod method;
+        private String path;
+        private Function<HttpRequest, HttpResponse> handler;
+
+        public Builder() {}
+
+        public Builder route(HttpMethod method, String path) {
+            this.method = method;
+            this.path = path;
+            return this;
+        }
+
+        public Builder handler(Function<HttpRequest, HttpResponse> handler) {
+            this.handler = handler;
+            return this;
+        }
+
+        public RouteEntry build() {
+            return new RouteEntry(method, path, handler);
+        }
+    }
+
+    public boolean matches(HttpRequest request) {
+        return method == request.getMethod() && path.equals(request.getPath());
+    }
+
+    public Function<HttpRequest, HttpResponse> getHandler() {
+        return handler;
+    }
+}
