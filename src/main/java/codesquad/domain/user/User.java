1f53bed1e64a64d81e0a5f8abd1ef02417949962
seminchoi <choicco89@gmail.com>
2024-07-05 00:00:30 +0900

    feat: User 클래스 구현
    -id, password, name 값을 가짐

---
+++ src/main/java/codesquad/domain/user/User.java
@@ -0,0 +1,25 @@
+package codesquad.domain.user;
+
+public class User {
+    private String userId;
+    private String password;
+    private String name;
+
+    public User(String userId, String password, String name) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
