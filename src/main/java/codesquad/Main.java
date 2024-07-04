3512f5d1082a1f7ac8e2526938f0b1103e2e010f
seminchoi <choicco89@gmail.com>
2024-07-05 01:28:16 +0900

    refactor: 구조 수정에 따른 Main 클래스 기능 변경

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -1,27 +1,14 @@
 package codesquad;
 
-import codesquad.config.ResourcePathManager;
+import codesquad.config.ObjectContainer;
 import codesquad.http.HttpServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        logger.info("Listening for connection on port 8080 ....");
-
-        HttpServer httpServer = new HttpServer(new ResourcePathManager());
-
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
-            Socket clientSocket = serverSocket.accept(); // 클라이언트 연결을 수락합니다.
-            httpServer.service(clientSocket);
-        }
+        HttpServer httpServer = new HttpServer(8080, 10, ObjectContainer.getRequestHandler());
+        httpServer.service();
     }
 }
