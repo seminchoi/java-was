6bf5d73709a204f7c06b60a0bd9aadd6901ae946
seminchoi <choicco89@gmail.com>
2024-07-05 01:16:35 +0900

    feat: 객체 인스턴스를 싱글턴으로 관리하고 반환받을 수 있는 ObjectContainer 구현

---
+++ src/main/java/codesquad/config/ObjectContainer.java
@@ -0,0 +1,30 @@
+package codesquad.config;
+
+import codesquad.domain.UserStorage;
+import codesquad.http.RequestHandler;
+import codesquad.usecase.StaticResourceMappingUsecase;
+import codesquad.usecase.UserUsecase;
+
+public class ObjectContainer {
+    private static final ObjectFactory objectFactory = new ObjectFactory();
+    private static final UserUsecase userUsecase = objectFactory.userUsecase();
+    private static final UserStorage userStorage = objectFactory.userStorage();
+    private static final RequestHandler requestHandler = objectFactory.requestHandler();
+    private static final StaticResourceMappingUsecase staticResourceMappingUsecase = objectFactory.requestMappingUsecase();
+
+    public static UserUsecase getUserUsecase() {
+        return userUsecase;
+    }
+
+    public static RequestHandler getRequestHandler() {
+        return requestHandler;
+    }
+
+    public static StaticResourceMappingUsecase getStaticResourceMappingUsecase() {
+        return staticResourceMappingUsecase;
+    }
+
+    public static UserStorage getUserStorage() {
+        return userStorage;
+    }
+}
