df58cb93bb58d6eae51391745a6cd9ab22ee21be
seminchoi <choicco89@gmail.com>
2024-07-05 01:07:22 +0900

    feat: 라우터에 등록할 엔트리를 관리하는 RouteEntryManager 클래스 구현

---
+++ src/main/java/codesquad/config/RouteEntryManager.java
@@ -0,0 +1,27 @@
+package codesquad.config;
+
+import codesquad.http.HttpMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteEntryManager {
+    private final List<RouteEntry> routeEntry = new ArrayList<>();
+
+    public RouteEntryManager() {
+        routeEntry.add(
+                new RouteEntry.Builder().route(HttpMethod.GET, "/user/create")
+                        .handler((httpRequest) -> ObjectContainer.getUserUsecase().createUser(httpRequest))
+                        .build()
+        );
+        routeEntry.add(
+                new RouteEntry.Builder().route(HttpMethod.GET, "/registration")
+                        .handler((httpRequest) -> ObjectContainer.getRequestMappingUsecase().register())
+                        .build()
+        );
+    }
+
+    public List<RouteEntry> getRouteEntry() {
+        return routeEntry;
+    }
+}
