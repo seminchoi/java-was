6a11884c4dc963754ce1959ea8f86571a626d199
seminchoi <choicco89@gmail.com>
2024-07-08 19:48:58 +0900

    test: HttpRequest 생성자 변경에 맞게 테스트 코드 수정

--- src/test/java/codesquad/http/HttpRequestTest.java
+++ src/test/java/codesquad/http/HttpRequestTest.java
@@ -2,34 +2,35 @@ package codesquad.http;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HttpRequestTest {
     @Test
     public void Http_request_header를_읽고_해석할_수_있다() {
-        List<String> header = List.of(
-                "GET /index.html HTTP/1.1",
-                "Host: www.example.com",
-                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                "Accept-Language: en-US,en;q=0.5",
-                "Accept-Encoding: gzip, deflate, br",
-                "Connection: keep-alive",
-                "Upgrade-Insecure-Requests: 1",
-                "Cache-Control: max-age=0"
-        );
+        String header =
+                "POST /user/create HTTP/1.1\r\n" +
+                        "Host: www.example.com\r\n" +
+                        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\n" +
+                        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8\r\n" +
+                        "Accept-Language: en-US,en;q=0.5\r\n" +
+                        "Accept-Encoding: gzip, deflate, br\r\n" +
+                        "Connection: keep-alive\r\n" +
+                        "Content-Length: 37\r\n" +
+                        "\r\n" +
+                        "userId=semin&name=semin&password=1234";
 
-        HttpRequest request = new HttpRequest(header);
+        InputStream inputStream = new ByteArrayInputStream(header.getBytes());
 
-        assertThat(request.getMethod()).isEqualTo(HttpMethod.GET);
-        assertThat(request.getPath()).isEqualTo("/index.html");
+        HttpRequest request = HttpRequestParser.parseRequest(inputStream);
+
+        assertThat(request.getMethod()).isEqualTo(HttpMethod.POST);
+        assertThat(request.getPath()).isEqualTo("/user/create");
         assertThat(request.getHttpVersion()).isEqualTo("HTTP/1.1");
         assertThat(request.getHost()).isEqualTo("www.example.com");
         assertThat(request.getConnection()).isEqualTo("keep-alive");
-        assertThat(request.getAccept()).isEqualTo("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-        assertThat(request.getHeader("Cache-Control")).isEqualTo("max-age=0");
-
+        assertThat(request.getBody()).isEqualTo("userId=semin&name=semin&password=1234");
     }
 }
