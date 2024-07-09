574957fa42ac7db1504efdfe4391dcfc41a1e25a
seminchoi <choicco89@gmail.com>
2024-07-09 13:49:09 +0900

    feat: HttpRequest 사용하지 않는 생성자 제거

--- src/main/java/codesquad/http/HttpRequest.java
+++ src/main/java/codesquad/http/HttpRequest.java
@@ -1,11 +1,9 @@
 package codesquad.http;
 
-import codesquad.exception.HttpException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Map;
 
 public class HttpRequest {
@@ -26,10 +24,6 @@ public class HttpRequest {
         this.queryParams = new Params(uri.getQuery());
         this.httpVersion = httpVersion;
         this.headers = headers;
-    }
-
-    public HttpRequest(List<String> strings) {
-        throw new HttpException(HttpStatus.BAD_REQUEST);
     }
 
     public void writeBody(String body) {
