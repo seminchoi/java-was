8c74761658b329720adc4cd460def5cbcf80d903
seminchoi <choicco89@gmail.com>
2024-07-09 21:50:49 +0900

    feat: HttpStatus UnAuthorized 상태 코드 추가

--- src/main/java/codesquad/server/http/HttpStatus.java
+++ src/main/java/codesquad/server/http/HttpStatus.java
@@ -7,8 +7,9 @@ public enum HttpStatus {
     SEE_OTHER(303, "See Other"),
 
     BAD_REQUEST(400, "Bad Request"),
-    CONFLICT(409, "Conflict"),
+    UNAUTHORIZED(401, "Unauthorized"),
     NOT_FOUND(404, "Not Found"),
+    CONFLICT(409, "Conflict"),
 
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),;
 
