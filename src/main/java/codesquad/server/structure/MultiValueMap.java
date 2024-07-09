76feb495df3912582e3ff0ba5f1634ad0be0cb2b
seminchoi <choicco89@gmail.com>
2024-07-09 16:55:04 +0900

    feat: 하나의 key에 여러개의 value를 저장할 수 있는 MutliValueMap 자료구조 구현

---
+++ src/main/java/codesquad/server/structure/MultiValueMap.java
@@ -0,0 +1,32 @@
+package codesquad.server.structure;
+
+import java.util.*;
+
+public class MultiValueMap <K, V> {
+    private Map<K, List<V>> map = new HashMap<>();
+
+    public void put(K key, V value) {
+        List<V> values = map.getOrDefault(key, new ArrayList<>());
+        values.add(value);
+    }
+
+    public V getFirst(K key) {
+        List<V> values = map.get(key);
+        if (values == null || values.isEmpty()) {
+            return null;
+        }
+        return values.get(0);
+    }
+
+    public List<V> get(K key) {
+        List<V> values = map.get(key);
+        if (values == null || values.isEmpty()) {
+            return null;
+        }
+        return map.getOrDefault(key, new ArrayList<>()).stream().toList();
+    }
+
+    public Set<K> keySet() {
+        return map.keySet();
+    }
+}
