d8cd4e9d089a94e77a7229f453f7cdce53651c6c
seminchoi <choicco89@gmail.com>
2024-07-10 15:31:22 +0900

    feat: key가 없을 때에만 저장하도록 수정

--- src/main/java/codesquad/server/structure/Params.java
+++ src/main/java/codesquad/server/structure/Params.java
@@ -1,8 +1,5 @@
 package codesquad.server.structure;
 
-import codesquad.exception.HttpException;
-import codesquad.server.http.HttpStatus;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,20 +7,25 @@ public class Params {
     private final Map<String, String> params = new HashMap<>();
 
     public Params(String query) {
-        if(query == null) {
+        if (query == null) {
             return;
         }
         String[] pairs = query.split("&");
         for (String pair : pairs) {
-            String[] splitPair = pair.split("=");
-            if (splitPair.length != 2) {
-                throw new HttpException(HttpStatus.BAD_REQUEST);
-            }
-            if (params.containsKey(splitPair[0])) {
-                params.put(splitPair[0], params.get(splitPair[0]) + "," + splitPair[1]);
+            int splitIndex = pair.indexOf("=");
+            if (splitIndex == -1) {
                 continue;
             }
-            params.put(splitPair[0], splitPair[1]);
+
+            String key = pair.substring(0, splitIndex);
+            String value = null;
+            if (pair.length() > splitIndex + 1) {
+                value = pair.substring(splitIndex + 1);
+            }
+
+            if (value != null && !params.containsKey(key)) {
+                params.put(key, value);
+            }
         }
     }
 
