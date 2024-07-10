2711b29066db61c17aff1d7e8a50161f2c992b4f
seminchoi <choicco89@gmail.com>
2024-07-10 19:38:38 +0900

    test: HttpServerTest 파일 위치 변경

--- src/test/java/codesquad/server/HttpServerTest.java
+++ src/test/java/codesquad/server/HttpServerTest.java
@@ -3,6 +3,8 @@ package codesquad.server;
 import org.junit.jupiter.api.Test;
 
 public class HttpServerTest {
+    private HttpServer httpServer;
+
     @Test
     void 요청이_끝나면_socket을_잘_닫는지_확인() {
     }
