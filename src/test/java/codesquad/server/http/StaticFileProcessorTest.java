41f28c038952b695d84268607c869e364c06a0f8
seminchoi <choicco89@gmail.com>
2024-07-10 19:37:41 +0900

    refactor: 필요없는 import 제거

--- src/test/java/codesquad/server/http/StaticFileProcessorTest.java
+++ src/test/java/codesquad/server/http/StaticFileProcessorTest.java
@@ -2,8 +2,6 @@ package codesquad.server.http;
 
 import codesquad.server.router.StaticFilePathManager;
 import codesquad.server.router.StaticFileProcessor;
-import codesquad.server.http.ContentType;
-import codesquad.server.http.HttpResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
