35b3160ad5f78bab77956a28aa2bc8bef8af7415
seminchoi <choicco89@gmail.com>
2024-07-17 10:55:46 +0900

    feat: 모델 객체 생성자 validation 추가

--- src/main/java/codesquad/app/model/Post.java
+++ src/main/java/codesquad/app/model/Post.java
@@ -1,5 +1,8 @@
 package codesquad.app.model;
 
+import codesquad.exception.HttpException;
+import codesquad.http.HttpStatus;
+
 public class Post {
     private Long id;
     private String title;
@@ -7,6 +10,9 @@ public class Post {
     private String authorId;
 
     public Post(Long id, String title, String content, String authorId) {
+        validateTitle(title);
+        validateContent(content);
+
         this.id = id;
         this.title = title;
         this.content = content;
@@ -17,6 +23,18 @@ public class Post {
         this.title = title;
         this.content = content;
         this.authorId = authorId;
+    }
+
+    private void validateTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "제목이 비어있습니다.");
+        }
+    }
+
+    private void validateContent(String content) {
+        if (content == null || content.isEmpty()) {
+            throw new HttpException(HttpStatus.BAD_REQUEST, "내용이 비어있습니다.");
+        }
     }
 
     public Long getId() {
